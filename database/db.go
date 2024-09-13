6d5f19de8035c49f4e43de4d3a95a1dedb743bbb
João Lucas <122113495+Joaolucas00@users.noreply.github.com>
2024-09-13 15:51:36 -0300

    CI (#1)
    * CI
    
    * Usando action de golangci-lint
    
    * Expondo a porta do banco de dados
    
    * Utilizando secrets
    
    ---------
    
    Co-authored-by: Joao Lucas Dorneles Avila <joao@DESKTOP-0VNSH8C>

--- database/db.go
+++ database/db.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"log"
-
+	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,11 +14,16 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	usuario := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	nameDB := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	stringDeConexao := "host=" + host + " user=" + usuario + " password=" + password + " dbname=" + nameDB + " port=" + dbPort + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
 	}
 
-	DB.AutoMigrate(&models.Aluno{})
+	_ = DB.AutoMigrate(&models.Aluno{})
 }
